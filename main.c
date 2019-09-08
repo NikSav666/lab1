f8c57cbbc1917e27f732ea086cd5fe86dfd650f8
Niksav666 <niktia-savin3@mail.ru>
2019-09-08 22:21:23 +0300

    div added in branch

--- main.c
+++ main.c
@@ -6,6 +6,6 @@ int main()
 	int a,b;
 	printf ("enter two numbers: ");
 	scanf("%d\n%d", &a, &b);
-	printf("sum = %d \ndiff = %d\n", a + b, a - b);
+	printf("sum = %d \ndiff = %d\ndiv = %d\n", a + b, a - b, a / b);
 	return 0;
 }
